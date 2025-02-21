449e72b266d101b1a0cec81e68295ba595aaf5da
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-02-21 13:14:22 +0530

    Forgot password, reset password, verify otp forms done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router'
 import Dashboard from './components/dashboard/Dashboard'
 import LoginForm from './components/forms/LoginForm'
 import RegistrationForm from './pages/forms/RegistrationForm'
+import ForgetPassword from './pages/forms/ForgotPassword'
+import ResetPassword from './pages/forms/ResetPassword'
+import VerifyOTP from './pages/forms/VerifyOTP'
 
 const App = () => {
   const routes =  createBrowserRouter([
     { path:'/', element:<Dashboard/>},
     { path: '/login', element:<LoginForm/>},
-    { path: '/register', element: <RegistrationForm/> }
+    { path: '/register', element: <RegistrationForm/> },
+    { path: '/forgot-password', element: <ForgetPassword/> },
+    { path: '/reset-password', element: <ResetPassword/> },
+    { path: '/verify-otp', element: <VerifyOTP/> },
   ])
   return (
     <>
