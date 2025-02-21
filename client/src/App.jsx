1b02a729c447c82a77fd859d3402cb7e5dfd5b12
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-02-21 12:20:51 +0530

    Login form and Registration form done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Dashboard from './components/dashboard/Dashboard'
+import LoginForm from './components/forms/LoginForm'
+import RegistrationForm from './pages/forms/RegistrationForm'
 
 const App = () => {
   const routes =  createBrowserRouter([
-    { path:'/', element:<Dashboard/>}
+    { path:'/', element:<Dashboard/>},
+    { path: '/login', element:<LoginForm/>},
+    { path: '/register', element: <RegistrationForm/> }
   ])
   return (
     <>
---
+++ client/src/components/forms/RadioButtonGroup.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+
+const RadioButtonGroup = ({
+  label,
+  name,
+  options,
+  selectedValue,
+  onChange,
+  required
+}) => {
+  return (
+    <div className="flex flex-col gap-2">
+      {label && (
+        <label className="text-sm font-medium text-primary-text">
+          {label} {required && <span className="text-red-500">*</span>}
+        </label>
+      )}
+
+      <div className="flex items-center gap-4">
+      {options.map((option) => {
+        const value = typeof option === 'object' ? option.value : option;
+        const label = typeof option === 'object' ? option.label : option;
+        
+        return (
+          <label key={value} className={`flex items-center gap-2 cursor-pointer`}>
+            <input
+              type="radio"
+              name={name}
+              value={value}
+              checked={selectedValue === value}
+              onChange={() => onChange(value)}
+              required={required}
+            />
+            <span className="text-sm text-gray-700">{label}</span>
+          </label>
+        );
+      })}
+      </div>
+    </div>
+  );
+};
+
+RadioButtonGroup.propTypes = {
+    label: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    options: PropTypes.arrayOf(
+      PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.shape({
+          label: PropTypes.string.isRequired,
+          value: PropTypes.string.isRequired,
+        }),
+      ])
+    ).isRequired,
+    selectedValue: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    required: PropTypes.bool,
+  };
+
+export default RadioButtonGroup;
